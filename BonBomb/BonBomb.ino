0c4dc08890f60a8d3c26b5854f7157086fa1714c
CoolCharle <charlietempist@hotmail.com>
2023-08-08 14:43:34 +1000

    Began writing the code

---
+++ BonBomb/BonBomb.ino
@@ -0,0 +1,47 @@
+// BonBomb Main
+
+// Variables
+const int ledPin = 13;
+const int buzzerPin = 9;
+
+int currentValue = 0;
+int timerValue = 0;
+unsigned long stopwatchValue = 0;
+int alarmValue = 0;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
+  digitalWrite(ledPin, LOW); // Turn off the LED initially
+}
+
+void flashValueOnScreen(int value) {
+  // Flash the value on the LED screen
+  for (int i=0; i<4; i++) {
+    digitalWrite(ledPin,HIGH); // Turn on the LED
+    delay(500); // Keep the LED on for half a second
+    digitalWrite(ledPin,LOW); // Turn off the LED
+    delay(500);
+  }
+}
+
+void onFirstStartOrReset() {
+  // Flash 4x 0 on screen
+  flashValueOnScreen(0);
+
+  // Reset all variables
+  currentValue = 0;
+  timerValue = 0;
+  stopwatchValue = 0;
+  alarmValue = 0;
+}
+
+void numberbutton() {
+  //
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
