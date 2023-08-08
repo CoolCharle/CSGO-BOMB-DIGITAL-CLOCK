a1e4807effde517f336d4461a559b7c5e1b1f44c
Yash SHETTY <she0067@mhs.vic.edu.au>
2023-08-08 13:46:57 +1000

    Added main.ino

---
+++ main/main.ino
@@ -0,0 +1,17 @@
+// BonBomb - Main 0.0
+
+/*
+
+ Initialise buttons here
+
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
